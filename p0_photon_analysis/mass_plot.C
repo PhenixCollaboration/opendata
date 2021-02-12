17c69f3c6c74b0428ed986c789acc942a2c1191d
Maxim Potekhin <potekhin@bnl.gov>
2021-02-11 20:24:39 -0500

    Updated the ROOT macro

--- p0_photon_analysis/mass_plot.C
+++ p0_photon_analysis/mass_plot.C
@@ -1,17 +1,31 @@
-void mass_plot() {
-  TFile* f1 = new TFile("MBntup.root");
-  TFile* f2 = new TFile("ERTntup.root");
-  
+
+void mass_plot(TString MBntup, TString ERTntup) {
+  TFile* f1 = new TFile(MBntup);
+  TFile* f2 = new TFile(ERTntup);
+
+  TCanvas *canvas  = new TCanvas("canvas",  "pi0 and gammas", 900, 900);
+  canvas->Divide(1,3);
+
+  //  f1->ls();
   f1->cd();
+  canvas_1->Divide(2,1);
+  canvas_2->Divide(2,1);
+  canvas_1_1->cd();
   ggntuple->Draw("mass","mass<1.0");
 
   f2->cd();
+  canvas_1_2->cd();
   ggntuple->Draw("mass","mass<1.0");
 
   f1->cd();
+  canvas_2_1->cd();
   ggntuple->Draw("mass>>htemp1","mass<1.0");
-  ggntuple->Draw("mass>>htemp2","mass<0.4&&chisq1<3.0&&chisq2<3.0");
+  canvas_2_2->cd();
+  ggntuple->Draw("mass>>htemp2","mass<1.0&&chisq1<3.0&&chisq2<3.0");
   
+  canvas_3->SetLogy();
+  canvas_3->cd();
+
   htemp2->SetLineColor(2);
   htemp1->Draw();
   htemp2->Draw("same");
